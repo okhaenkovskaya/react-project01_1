04796c22acc44e50098a4507ed94f8edcb8f0f87
Oksana Prokopenko <oksana.prokopenko@p2h.com>
2023-01-16 11:37:38 +0400

    update server holder

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,4 +1,5 @@
 import bcrypt from "bcryptjs";
+import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
 
 import User from "../models/user.js";
@@ -21,11 +22,7 @@ export const login = async (req, res) => {
       return res.status(400).json({ message: "Invalid Credentials" });
 
     const token = jwt.sign(
-      {
-        email: existingUser.email,
-        id: existingUser._id,
-        fullName: existingUser.fullName,
-      },
+      { email: existingUser.email, id: existingUser._id },
       "some very secret key",
       { expiresIn: "1h" }
     );
@@ -58,22 +55,67 @@ export const registration = async (req, res) => {
     });
 
     const token = jwt.sign(
-      { email: result.email, id: result._id, fullName: result.fullName },
+      { email: result.email, id: result._id },
       "some very secret key",
       { expiresIn: "1h" }
     );
 
-    res
-      .status(200)
-      .json({
-        result: {
-          _id: result._id,
-          fullName: result.fullName,
-          email: result.email,
-        },
-        token,
-      });
+    res.status(200).json({
+      result: {
+        _id: result._id,
+        fullName: result.fullName,
+        email: result.email,
+      },
+      token,
+    });
   } catch (error) {
     res.status(500).json({ message: "Something went wrong" });
   }
+};
+
+export const deleteUser = async (req, res) => {
+  const user = await User.findById(req.params.id);
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+
+  await user.remove();
+
+  return res.json({ message: "User deleted" });
+};
+
+export const updateUserProfile = async (req, res) => {
+  const { id: _id } = req.params;
+  const profile = req.body;
+  if (!mongoose.Types.ObjectId.isValid(_id))
+    return res.status(404).send("User not found");
+  const updatedProfile = await User.findByIdAndUpdate(
+    _id,
+    { ...profile, _id },
+    { new: true }
+  );
+  res.json(updatedProfile);
+};
+
+export const getUser = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = await User.findById(id);
+    res.status(200).json(user);
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const getUsers = async (req, res) => {
+  const users = await User.find();
+  const privateFields = users.map((user) => {
+    return {
+      _id: user._id,
+      fullName: user.fullName,
+      email: user.email,
+    };
+  });
+  return res.json(privateFields);
 };
